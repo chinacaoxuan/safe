4e0759d2f741b4a1902f4cd295e7a2385f37a4f8
Demon <sheepcaoxuan@163.com>
2017-07-17 17:56:38 +0800

    add file

---
+++ NSDictionary+Safe.h
@@ -0,0 +1,17 @@
+//
+//  NSDictionary+Safe.h
+//  demon
+//
+//  Created by demon on 2017/4/1.
+//  Copyright © 2017年 demon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (Safe)
+
+@end
+
+@interface NSMutableDictionary (Safe)
+
+@end
